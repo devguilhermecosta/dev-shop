7790256d56ae4181006d70bedfd587cb437915d4
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-06 22:19:32 -0300

    feat: created the json-server and axios to render the products

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,52 @@
 import './home.modules.css';
 import { BsCart } from 'react-icons/bs';
+import { api } from '../../services/api';
+import { useState, useEffect } from 'react';
+
+interface ProductProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 
 export default function Home() {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    getProducts();
+
+  }, [])
+
   return (
     <main className="C-main">
       <h1 className="C-main__title">Produtos em alta</h1>
 
       <section className="C-products">
 
-        <section className="C-product">
-          <div className="C-product_c_img">
-            <img src="https://media.pichau.com.br/media/catalog/product/cache/2f958555330323e505eba7ce930bdf27/p/g/pgk-p432vn-rgb0100110.jpg" alt="teclado" />
-          </div>
-          <div className="C-product_details">
-            <h2>Nome do produto</h2>
-            <p>
-              R$ 150,00
-              <span><BsCart size={20} color="#ffffff"/></span>
-            </p>
-          </div>
-        </section>
+        {products.map((product) => (
+          <section className="C-product" key={product.id}>
+            <div className="C-product_c_img">
+              <img src={product.cover} alt={product.title} />
+            </div>
+            <div className="C-product_details">
+              <h2>{product.title}</h2>
+              <p>
+                {product.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL"
+                })}
+                <span><BsCart size={20} color="#ffffff"/></span>
+              </p>
+            </div>
+          </section>
+        ))}
   
       </section>
     </main>
